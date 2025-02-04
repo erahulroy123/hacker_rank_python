d23d5cc912a31f4074431751ff5eaff525f1c6f6
Rahul Roy <erahulroy123@gmail.com>
2025-02-05 04:46:56 +0530

    Update if_else.py

--- introduction/if_else.py
+++ introduction/if_else.py
@@ -1,7 +1,7 @@
 n = int(input())
-if n % 2 != 0:
+if n % 2 != 0: #reminder
     print('Weird')
-elif n % 2 == 0 and 2 <= n <= 5:
+elif n % 2 == 0 and 2 <= n <= 5: #n is between 2 and 5
     print('Not Weird')
 elif n % 2 == 0 and 6 <= n <= 20:
     print('Weird')
